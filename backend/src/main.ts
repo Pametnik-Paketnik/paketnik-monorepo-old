9ef5c46c2ce34731b7a5d9f2a0135bcca61b7789
pegi4 <vito.senic@gmail.com>
2025-05-12 15:31:36 +0200

    added swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Paketnik')
+    .setDescription('Paketnik API description')
+    .setVersion('1.0')
+    .addTag('paketnik')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
