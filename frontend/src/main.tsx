480f6192a81b868c65c6b690ebe363032d6a8fd9
pegi4 <vito.senic@gmail.com>
2025-06-05 10:39:43 +0200

    KAN-10 removed redundant routes in the main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,71 +1,17 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
 import './index.css'
 import App from './App'
-import Home from './routes/Home'
-import About from './routes/About'
-import Login from './routes/Login'
-import Register from './routes/Register'
-import Boxes from './routes/Boxes'
-import Reservations from './routes/Reservations'
-import { ProtectedRoute } from '@/components/protected-route'
-
-const router = createBrowserRouter([
-  {
-    path: '/*',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: (
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: 'about',
-        element: (
-          <ProtectedRoute>
-            <About />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: 'boxes',
-        element: (
-          <ProtectedRoute>
-            <Boxes />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: 'reservations',
-        element: (
-          <ProtectedRoute>
-            <Reservations />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: 'login',
-        element: <Login />,
-      },
-      {
-        path: 'register',
-        element: <Register />,
-      },
-    ],
-  },
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
